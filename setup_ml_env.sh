772c848f251fedded3e062d88d8cccb1a1ada945
Sergey Ivanychev <sergeyivanychev@gmail.com>
2018-03-02 17:52:25 +0300

    Launching screen at the end.

--- setup_ml_env.sh
+++ setup_ml_env.sh
@@ -3,13 +3,14 @@ cd Downloads
 apt-get update
 apt-get upgrade -y
 # For Anaconda installation.
-apt-get install wget bzip2 -y
+apt-get install wget bzip2 screen -y
 source ~/.bashrc
 
 wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O anaconda.sh
 bash anaconda.sh
 source ~/.bashrc
 
+sudo chown -R $USER anaconda3
 conda update conda -y
 conda update anaconda -y
 
@@ -22,3 +23,5 @@ echo "c = get_config()
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8123" >> ~/.jupyter/jupyter_notebook_config.py
+
+screen -S jupyter
