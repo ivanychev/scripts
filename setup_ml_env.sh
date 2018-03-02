61227200308c5032400ad0b7400270bea0798d1b
Sergey Ivanychev <sergeyivanychev@gmail.com>
2018-03-02 17:26:40 +0300

    Turn off silent mode

--- setup_ml_env.sh
+++ setup_ml_env.sh
@@ -7,9 +7,7 @@ apt-get install wget bzip2 -y
 source ~/.bashrc
 
 wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O anaconda.sh
-bash anaconda.sh -b -p $HOME/anaconda
-export PATH="$HOME/anaconda/bin:$PATH"
-source ~/.bashrc
+bash anaconda.sh
 
 conda update conda
 conda update anaconda
